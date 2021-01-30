e6d6cb12053da0cdd31cee4f09b02d139dc888e2
Julian Cubillos <juliandavid25@hotmail.com>
2021-01-29 19:36:26 -0500

    Resolves challenges days 4, 5 and 6

---
+++ day4Classes.js
@@ -0,0 +1,25 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+/* In the following function I write down the HackerRank challenge, in the previous lines HackerRank establishes the code.*/
+function Polygon(a) {
+    this.perimeter = function() {
+        let perimeterP = 0
+        for (let i = 0; i < a.length; i++) {
+            perimeterP = perimeterP + a[i]
+        }
+        return perimeterP
+    }
+}
+
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
